b0558eed305d4912b07242f5a6deb64030f1d364
CaioLima <caiohenrys50@gmail.com>
2026-04-11 13:19:21 -0300

    config knex

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
+import { clientKnex } from './database.js'
 
 const app = fastify()
 
-app.get('/', () => {
-  console.log('hello world')
+app.get('/', async () => {
+  const tables = await clientKnex('sqlite_schema').select('*')
+  return tables
 })
 
 app
