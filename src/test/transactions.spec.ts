8feca2ac8fa81a72fba6830640ea78c2927459a8
CaioLima <caiohenrys50@gmail.com>
2026-04-21 14:26:31 -0300

    test lists transactions

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { app } from '../app.js'
 
 describe('Transactions routes', async () => {
@@ -24,7 +24,32 @@ describe('Transactions routes', async () => {
       .expect(201)
   })
 
-  // it('should be possible to list all transactions', () => {
+  it('should be possible to list all transactions', async () => {
+    const createTransactionsResponse = await server
+      .post('/transactions')
+      .send({
+        text: 'New Transactions',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
 
-  // })
+    const cookies = createTransactionsResponse.get('Set-Cookie')
+
+    if (!cookies) {
+      throw new Error('Unauthozied')
+    }
+
+    const ListsTransactionsResponse = await server
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(ListsTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        text: 'New Transactions',
+        amount: 5000,
+      }),
+    ])
+  })
 })
